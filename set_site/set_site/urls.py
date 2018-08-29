f59867e5089ad68d6ce8fe290ab69e81c34c5c45
Sachin Gaikwad <gaikwad411@gmail.com>
2018-08-29 21:07:07 +0530

    bootstrap theme sbadmin v2.0 integrated

--- set_site/set_site/urls.py
+++ set_site/set_site/urls.py
@@ -15,8 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static
+from django.conf import settings
+
 
 urlpatterns = [
     path('', include('core.urls')),
     path('admin/', admin.site.urls),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
